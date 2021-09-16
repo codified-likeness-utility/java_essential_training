5736fd76765dd62497bede6000ba8ea58250ecd6
Ian Davies <mr.daviesian@gmail.com>
2021-09-16 13:53:13 -0500

    create loops

--- src/Main.java
+++ src/Main.java
@@ -8,40 +8,44 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        var now = LocalDateTime.now();
-        var monthNumber = now.getMonthValue();
+        String[] months = {
+                "January",
+                "February",
+                "March",
+                "April",
+                "may",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+        };
 
-        switch (monthNumber) {
-            case 1:
-                System.out.println("The month is January!");
-            case 2:
-                System.out.println("The month is February!");
-            case 3:
-                System.out.println("The month is March!");
-                break;
-            case 4:
-                System.out.println("The month is April!");
-                break;
-            case 5:
-                System.out.println("The month is May!");
-                break;
-            case 6:
-                System.out.println("The month is June!");
-                break;
-            case 7:
-            case 8:
-            case 9:
-                System.out.println("Hot summer's lead to beautiful fall leaves!");
-                break;
-            case 10:
-            case 11:
-            case 12:
-                System.out.println("It's Wintertime!!");
-                break;
-            default:
-                System.out.println("You chose another month...");
+        // Iterative loop
+//        for (int i = 0; i < months.length; i++) {
+//            System.out.println(months[i]);
+//        }
 
-        }
+        // forEach loop
+//        for (String month : months) {
+//            System.out.println(month);
+//        }
+
+        // While loop
+//        var whileCounter = 0;
+//        while (whileCounter < months.length) {
+//            System.out.println(months[whileCounter]);
+//            whileCounter++;
+//        }
+
+        // do loop
+        var doCounter = 0;
+        do {
+            System.out.println(months[doCounter]);
+            doCounter++;
+        } while (doCounter < months.length);
     }
 }
 
